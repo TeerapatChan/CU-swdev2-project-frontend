11ba10dae6f38314def5fa02adc33052380fd08c
Teerapat Chantaramanee <teerapatchantaramanee@gmail.com>
2023-10-28 22:06:12 +0700

    ✨feat: CustomTextfield

--- src/components/CustomTextField.tsx
+++ src/components/CustomTextField.tsx
@@ -4,16 +4,21 @@ import { Controller } from 'react-hook-form';
 export default function CustomTextField({ props }: { props: any }) {
   return (
     <Controller
-      name='name'
+      name={props.label}
       control={props.control}
+      rules={{ required: true }}
       render={({ field }) => (
         <TextField
           {...field}
-          label='Name'
           variant='outlined'
-          type='text'
-          error={!!props.errors.`${props.label}`?.message}
-          helperText={props.name ? props.name?.message : ''}
+          type={props.type ? props.type : 'text'}
+          label={props.label.charAt(0).toUpperCase() + props.label.slice(1)}
+          error={!!props.errors[props.label]?.message}
+          helperText={
+            props.errors[props.label] ? props.errors[props.label]?.message : ''
+          }
+          size='small'
+          fullWidth
         />
       )}
     />
