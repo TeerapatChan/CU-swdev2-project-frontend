30ff01fec43f85dea7867d2e4cccdc9a6c2bd1dd
Teerapat Chantaramanee <teerapatchantaramanee@gmail.com>
2023-10-28 22:07:02 +0700

    🚧feat: Forms

--- src/components/forms/BookingForm.tsx
+++ src/components/forms/BookingForm.tsx
@@ -1,40 +1,61 @@
 'use client';
-import TextField from '@mui/material/TextField/TextField';
 import DateDentist from '../DateDentist';
 import Button from '@mui/material/Button';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { BookingSchema } from '@/utils/FormSchema';
+import CustomTextField from '../CustomTextField';
+import { useState } from 'react';
+import { Dayjs } from 'dayjs';
+import { BookingYup } from '@/utils/YupSchema';
 
 export default function BookingForm() {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<BookingSchema>({
+    resolver: yupResolver(BookingYup),
+    defaultValues: {
+      name: '',
+      email: '',
+      tel: '',
+    },
+  });
+  const [date, setDate] = useState<Dayjs | null>(null);
+  const [dentist, setDentist] = useState<string>('Select Dentist');
+  const formSubmit: SubmitHandler<BookingSchema> = (data: BookingSchema) => {
+    console.log(data);
+    console.log(date?.toString());
+    console.log(dentist);
+  };
+  console.log('errors', errors);
   return (
-    <div className='w-[700px] h-[550px] flex flex-col gap-4 items-start justify-center bg-white rounded-lg pr-24 pl-24'>
-      <p className=' text-3xl font-bold'>Request an appointment</p>
+    <form
+      className='w-[700px] h-[600px] flex flex-col gap-2 items-start justify-center 
+    bg-white rounded-lg pr-24 pl-24'
+      onSubmit={handleSubmit(formSubmit)}
+    >
+      <p className=' text-3xl font-bold self-center'>Request an appointment</p>
       <div className='w-full flex flex-col gap-2'>
         <p className='text-base'>Name</p>
-        <TextField
-          variant='outlined'
-          className='w-full bg-white'
-          size='small'
-        ></TextField>
+        <CustomTextField props={{ control, errors, label: 'name' }} />
       </div>
       <div className='w-full flex flex-col gap-2'>
         <p className='text-base'>Email</p>
-        <TextField
-          variant='outlined'
-          className='w-full bg-white'
-          size='small'
-        ></TextField>
+        <CustomTextField props={{ control, errors, label: 'email' }} />
       </div>
-      <div className='w-full flex flex-col gap-2'>
+      <div className='w-full flex flex-col gap-2 mb-2'>
         <p className='text-base'>Tel.</p>
-        <TextField
-          variant='outlined'
-          className='w-full bg-white'
-          size='small'
-        ></TextField>
+        <CustomTextField props={{ control, errors, label: 'tel' }} />
       </div>
-      <DateDentist />
-      <Button variant='contained' className='bg-sky-600 w-full'>
-        Book now
+      <DateDentist
+        onDateChange={(e) => setDate(e)}
+        onDentistChange={(e) => setDentist(e)}
+      />
+      <Button type='submit' variant='contained' className='bg-sky-600 w-full'>
+        Make an appointment
       </Button>
-    </div>
+    </form>
   );
 }
--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -1,22 +1,44 @@
-import { TextField } from '@mui/material';
+'use client';
 import Button from '@mui/material/Button';
 import Link from 'next/link';
+import { LoginYup } from '@/utils/YupSchema';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import CustomTextField from '../CustomTextField';
+import { LoginSchema } from '@/utils/FormSchema';
 
 export default function SignInForm() {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<LoginSchema>({
+    resolver: yupResolver(LoginYup),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const formSubmit = (data: LoginSchema) => {
+    console.log(data);
+  };
   return (
-    <div className='h-1/2 w-4/5 flex flex-col gap-5 items-center justify-center'>
+    <form
+      className='h-1/2 w-4/5 flex flex-col gap-5 items-center justify-center'
+      onSubmit={handleSubmit(formSubmit)}
+    >
       <div className='w-4/5 flex flex-col gap-5 items-start'>
-        <TextField
-          variant='outlined'
-          label='Email'
-          className='w-full'
-        ></TextField>
-        <TextField
-          variant='outlined'
-          label='Password'
-          className='w-full'
-        ></TextField>
-        <Button variant='contained' className='bg-sky-600 w-full' size='large'>
+        <CustomTextField props={{ control, errors, label: 'email' }} />
+        <CustomTextField
+          props={{ control, errors, label: 'password', type: 'password' }}
+        />
+        <Button
+          type='submit'
+          variant='contained'
+          className='bg-sky-600 w-full'
+          size='large'
+        >
           Log In
         </Button>
         <div className='self-center'>
@@ -30,6 +52,6 @@ export default function SignInForm() {
           </Link>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
