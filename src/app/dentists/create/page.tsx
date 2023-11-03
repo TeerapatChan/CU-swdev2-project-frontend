8eb2d94e77aa1bb95f96b0ccc9f17d7a9411bbfb
Teerapat Chantaramanee <teerapatchantaramanee@gmail.com>
2023-11-03 10:41:22 +0700

    ✨feat: create page

---
+++ src/app/dentists/create/page.tsx
@@ -0,0 +1,34 @@
+<<<<<<< Updated upstream
+||||||| Stash base
+import CreateDentistForm from '@/components/forms/CreateDentistForm';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+
+export default async function CreateDentistPage() {
+  const session = await getServerSession(authOptions);
+
+  return (
+    <div className="bg-[url('/img/main-bg.png')] h-[120vh] bg-cover flex justify-center items-center">
+      <CreateDentistForm />
+    </div>
+  );
+}
+=======
+import CreateDentistForm from '@/components/forms/CreateDentist/CreateDentistForm';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import getUserProfile from '@/libs/user/getUserProfile';
+
+export default async function CreateDentistPage() {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user.token) return null;
+  const profile = (await getUserProfile(session.user.token)).data;
+  if (profile.role !== 'admin') return null;
+
+  return (
+    <div className="bg-[url('/img/main-bg.png')] h-[120vh] bg-cover flex justify-center items-center">
+      <CreateDentistForm token={session.user.token} />
+    </div>
+  );
+}
+>>>>>>> Stashed changes
