25be2b06d8388d9aaba8c93c309011ef3b64f957
Teerapat Chantaramanee <teerapatchantaramanee@gmail.com>
2023-11-03 12:29:52 +0700

    🚑️feat: create page

--- src/app/dentists/create/page.tsx
+++ src/app/dentists/create/page.tsx
@@ -1,19 +1,3 @@
-<<<<<<< Updated upstream
-||||||| Stash base
-import CreateDentistForm from '@/components/forms/CreateDentistForm';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-
-export default async function CreateDentistPage() {
-  const session = await getServerSession(authOptions);
-
-  return (
-    <div className="bg-[url('/img/main-bg.png')] h-[120vh] bg-cover flex justify-center items-center">
-      <CreateDentistForm />
-    </div>
-  );
-}
-=======
 import CreateDentistForm from '@/components/forms/CreateDentist/CreateDentistForm';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
@@ -31,4 +15,3 @@ export default async function CreateDentistPage() {
     </div>
   );
 }
->>>>>>> Stashed changes
